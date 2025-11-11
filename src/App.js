51411a32e1a8258a7606e3aff92bb925c059b4c7
Baltazar Caceres <baltazar.caceres@correo.uady.mx>
2025-11-11 08:38:44 -0600

    Corrige importación de ReservationForm en App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ReservationForm from './components/ReservationForm';
+import ReservationForm from './components/forms/ReservationForm';
+
 import Home from './components/Home';
 import Menu from './components/Menu';
 import CartModal from './components/CartModal';
