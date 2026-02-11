3a02a2f1b1d8dfd26146da9b5537f7497c638b97
Baltazar Caceres <baltazar.caceres@correo.uady.mx>
2026-02-10 22:51:32 -0600

    Add BookingPage with BookingForm import

---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BookingForm from "../components/BookingForm.jsx";
+
+function BookingPage() {
+  return (
+    <div>
+      <h1>Reserva tu mesa</h1>
+      <BookingForm />
+    </div>
+  );
+}
+
+export default BookingPage;
